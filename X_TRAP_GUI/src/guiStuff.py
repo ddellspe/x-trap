a44808160d6e7742c50463b1419b1b4f06bb2c1d
david.dellsperger <david.dellsperger@gmail.com>
2013-06-21 10:15:46 -0500

    Revert "Took out an unused import"
    This reverts commit 7c60f60da46dd4b45952227a08df904040ff0ae8.

--- X_TRAP_GUI/src/guiStuff.py
+++ X_TRAP_GUI/src/guiStuff.py
@@ -20,6 +20,7 @@ import numpy
 from tkSimpleDialog import Dialog
 import webbrowser
 import os
+import sys
 import serialCommands
 import thread
 import moveCommands 
@@ -27,6 +28,7 @@ import HeightSelector
 import ConfigParser
 from numpy import *
 from scipy import *
+from math import *
 import AffineTransform # new affine transform
 import dicomViewer
 import traceback
